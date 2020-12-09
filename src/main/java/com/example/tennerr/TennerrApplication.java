21f7a75463d9ad9491e58b39b03e50fcacdebc73
Sandra Daubigne <44670927+SandraDaubigne@users.noreply.github.com>
2020-12-09 16:04:34 +0100

    new project

---
+++ src/main/java/com/example/tennerr/TennerrApplication.java
@@ -0,0 +1,13 @@
+package com.example.tennerr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TennerrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TennerrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tennerr/TennerrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tennerr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TennerrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
