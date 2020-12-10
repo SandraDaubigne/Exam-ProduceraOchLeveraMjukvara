e779f7758cb551d66a0e0089e664c41014190b92
Sandra Daubigne <44670927+SandraDaubigne@users.noreply.github.com>
2020-12-10 11:08:32 +0100

    validate rolles
    Fetch is validating roles and save right role

--- src/main/java/com/example/tennerr/EntityService.java
+++ src/main/java/com/example/tennerr/EntityService.java
@@ -9,33 +9,16 @@ public class EntityService {
     @Autowired
     private EntityRepository entityRepository;
 
-
     //2:Skapa Funktion för att kunna registrera sig
-
     public void addWorker(Worker worker){
         entityRepository.save(worker);
     }
 
-
-    public Worker registerWorker(String username, String password){
-
-        Worker worker = new Worker();
-        worker.setUsername(username);
-        worker.setPassword(password);
-
-        return entityRepository.save(worker);
-
-    }
-
     //2:Skapa Funktion för att kunna registrera sig
-    public Workgiver registerWorkgiver(String username, String password){
-
-        Workgiver workgiver = new Workgiver();
-        workgiver.setAddress(username);
-        workgiver.setPassword(password);
-
-        return entityRepository.save(workgiver);
-
+    public void addWorkgiver(Workgiver workgiver){
+        entityRepository.save(workgiver);
     }
+
+
 
 }
