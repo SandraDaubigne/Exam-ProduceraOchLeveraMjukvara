0869c138c6dc03005d729ef3d446fa009edfaa31
Sandra Daubigne <44670927+SandraDaubigne@users.noreply.github.com>
2020-12-10 10:55:10 +0100

    Username and password
    Username and password going to the DB

--- src/main/java/com/example/tennerr/EntityService.java
+++ src/main/java/com/example/tennerr/EntityService.java
@@ -9,8 +9,33 @@ public class EntityService {
     @Autowired
     private EntityRepository entityRepository;
 
-    public void addUser(User user){
-        entityRepository.save(user);
+
+    //2:Skapa Funktion för att kunna registrera sig
+
+    public void addWorker(Worker worker){
+        entityRepository.save(worker);
+    }
+
+
+    public Worker registerWorker(String username, String password){
+
+        Worker worker = new Worker();
+        worker.setUsername(username);
+        worker.setPassword(password);
+
+        return entityRepository.save(worker);
+
+    }
+
+    //2:Skapa Funktion för att kunna registrera sig
+    public Workgiver registerWorkgiver(String username, String password){
+
+        Workgiver workgiver = new Workgiver();
+        workgiver.setAddress(username);
+        workgiver.setPassword(password);
+
+        return entityRepository.save(workgiver);
+
     }
 
 }
