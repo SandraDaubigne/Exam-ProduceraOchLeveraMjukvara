ff26fbd5da1746055f5fa1aa835163e8bbc602dc
Sandra Daubigne <44670927+SandraDaubigne@users.noreply.github.com>
2020-12-10 11:10:07 +0100

    Clean

--- src/main/java/com/example/tennerr/HomeController.java
+++ src/main/java/com/example/tennerr/HomeController.java
@@ -3,8 +3,6 @@ package com.example.tennerr;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
