884afafeb2b2fc0cc16a19a139ad25c06f446378
Sandra Daubigne <44670927+SandraDaubigne@users.noreply.github.com>
2020-12-14 14:09:49 +0100

    cleaning

--- src/main/java/com/example/tennerr/HomeController.java
+++ src/main/java/com/example/tennerr/HomeController.java
@@ -1,8 +1,9 @@
 package com.example.tennerr;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class HomeController {
@@ -14,5 +15,26 @@ public class HomeController {
     public String home(){
         return "index";
     }
+
+    //2:Skapa Funktion för att kunna registrera sig
+    //Registrera arbetare
+    @RequestMapping(value = "/createworker",method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    public String createWorker(@RequestBody Worker worker)
+    {
+        entityService.addWorker(worker);
+        return "index";
+    }
+
+    //2:Skapa Funktion för att kunna registrera sig
+    //registrera arbetsgivare
+    @RequestMapping(value = "/createworkgiver",method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    public String createWorkgiver(@RequestBody Workgiver workgiver)
+    {
+        entityService.addWorkgiver(workgiver);
+        return "index";
+    }
+
+
+
 
 }
