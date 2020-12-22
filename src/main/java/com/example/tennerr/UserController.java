e161583091a8463f4b7199d63ba5a977373c4a09
Sandra Daubigne <44670927+SandraDaubigne@users.noreply.github.com>
2020-12-22 13:00:06 +0100

    register working

---
+++ src/main/java/com/example/tennerr/UserController.java
@@ -0,0 +1,39 @@
+package com.example.tennerr;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    //Startvyn
+    @GetMapping("/")
+    public String startPage(Model model){
+        model.addAttribute("list", userService.getAllUsers());
+        return "userstart";
+    }
+
+    //Show Form register
+    @GetMapping("/showNewUserForm")
+    public String registerUser(Model model){
+        UserEntity userEntity = new UserEntity();
+        model.addAttribute("user", userEntity);
+        return "userform";
+    }
+
+    //C - Create User
+    @PostMapping("/saveUser")
+    public String saveUser(@ModelAttribute("user") UserEntity  userEntity, Model model){
+        userService.saveUser(userEntity);
+        return "loggedin";
+    }
+
+
+}
---
+++ src/main/java/com/example/tennerr/UserService.java
@@ -0,0 +1,21 @@
+package com.example.tennerr;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<UserEntity> getAllUsers(){
+        return userRepository.findAll();
+    }
+
+    public void saveUser(UserEntity userEntity){
+        userRepository.save(userEntity);
+    }
+}
